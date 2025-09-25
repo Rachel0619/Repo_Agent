602290d41328c1fda3a6ebe1ea7388a12d3d476c
Runtian (Rachel) Li <Rachel_0619@hotmail.com>
2025-09-24 23:39:37 -0700

    initial commit

---
+++ read_repo.py
@@ -0,0 +1,43 @@
+import io
+import zipfile
+import requests
+import frontmatter
+
+def read_repo_data(reoi_owner, repo_name):
+    prefix = 'https://codeload.github.com'
+    url = f'{prefix}/{reoi_owner}/{repo_name}/zip/refs/heads/main'
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        raise Exception(f"Failed to download repository: {resp.status_code}")
+
+    repository_data = []    
+
+    zf = zipfile.ZipFile(io.BytesIO(resp.content))
+
+    for file_info in zf.infolist():
+        filename = file_info.filename
+        filename_lower = filename.lower()
+
+        if not (filename_lower.endswith('.md')
+            or filename_lower.endswith('.mdx')):
+            continue
+        
+        try:
+            with zf.open(file_info) as f_in:
+                content = f_in.read().decode('utf-8', errors='ignore')
+                post = frontmatter.loads(content)
+                data = post.to_dict()
+                data['filename'] = filename
+                repository_data.append(data)
+        except Exception as e:
+            print(f'Error processing {filename}: {e}')
+            continue
+    zf.close()
+    return repository_data
+
+if __name__ == "__main__":
+    owner = 'rachel0619'
+    repo = 'VanTrails'
+    data = read_repo_data(owner, repo)
+    print(data[1])
